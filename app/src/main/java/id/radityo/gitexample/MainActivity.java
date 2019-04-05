9a2f1fb68bb72dcc807b13e62de4b61ea1601d24
wradiy <wicakradityo778@gmail.com>
2019-04-05 17:12:20 +0700

    Commit ketujuh

--- app/src/main/java/id/radityo/gitexample/MainActivity.java
+++ app/src/main/java/id/radityo/gitexample/MainActivity.java
@@ -1,7 +1,11 @@
 package id.radityo.gitexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog builder = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Dialog")
+                        .setMessage("Yakin ingin keluar?")
+                        .setPositiveButton("ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("batal", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 }
